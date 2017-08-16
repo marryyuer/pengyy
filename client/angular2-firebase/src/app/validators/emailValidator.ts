e0cac639bce52c1f4865293aafadf531ff70c720
marrypen <664150686@qq.com>
2017-08-16 14:35:30 +0800

    fixed a bug for showing error while nothing is inputed.

--- client/angular2-firebase/src/app/validators/emailValidator.ts
+++ client/angular2-firebase/src/app/validators/emailValidator.ts
@@ -5,7 +5,7 @@ export class EmailValidator {
   static isValid(control: FormControl) {
     const regx = /^\w+@[0-9a-zA-Z_.]+?\.[a-zA-Z]{2,3}$/.test(control.value);
 
-    if (regx) {
+    if (regx || !control.value) {
       return null;
     }
 
